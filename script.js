7737f67119fcd40ef65a261bb5da1f64dd4085e6
jjolly04 <judejolly2004@gmail.com>
2024-07-08 18:24:25 -0500

    Create getComputerChoice function
    made the above function to return the computer's random choice for
    rock paper scissors. This is done by using math.random, floor,
    multiplying by 10, and then modulo 3 to get a remainder between 0 and
    2, and then assigning the computer's choice based on that random
    number.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+// create function to return computer choice of rock, paper, or scissors
+// initialize function with no arguments
+// use math.random to get a random num
+// multiply by 10  and then math.floor to get nums from 0 to 9 
+// modulo answer by 3 to get remainder from 0 - 2
+// return computer choice based on resulting num ie if 0 then return rock
+
+
+function getComputerChoice() {
+    let randomNum = (Math.floor(Math.random() * 10)) % 3;
+
+    if (randomNum === 0) {
+        return 'rock';
+    }
+    else if (randomNum === 1) {
+        return 'paper';
+    }
+    else {
+        return 'scissors';
+    }
+}
+
